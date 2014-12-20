40bb1b2c426e1a9d8ac629f5c7680decaf75226e
Jacob Kobza <jacobkobza2016@u.northwestern.edu>
2014-12-20 04:55:06 -0600

    Minor changes

--- js/app.js
+++ js/app.js
@@ -60,7 +60,8 @@ Player.prototype.handleInput = function(key) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
